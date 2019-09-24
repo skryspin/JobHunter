1911bb87e0b983a9f732bc09ae831c55b07e3d1f
skryspin <None>
2019-09-24 18:11:18 -0400

    Implemented player health and healthbar

---
+++ Assets/PlayerHealth.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+
+    public GameObject player;
+    private Player playerScript;
+    private Slider mySlider; 
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerScript = player.GetComponent<Player>();
+        mySlider = this.GetComponent<Slider>();
+        mySlider.minValue = 0;         
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mySlider.maxValue = playerScript.maxHealth;
+        mySlider.value = playerScript.currentHealth;
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,19 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
+
+    //Movement related
     private CharacterController characterController; 
     private GameObject my_camera; 
     private Vector3 lastMovement; 
     private int score;
+   
     
+    //Movement
     public float speed;
     public float jumpSpeed;
     public float gravity;
     public float pushPower; 
 
+    //Combat
+    public int currentHealth;
+    public int maxHealth;
+    public Slider healthBar;
+    public Button btn; 
 
     
     // Start is called before the first frame update
@@ -21,6 +31,7 @@ public class Player : MonoBehaviour
     {
         characterController = this.GetComponent<CharacterController>();
         my_camera = GameObject.FindWithTag("MainCamera");
+        btn.onClick.AddListener(() => TakeDamage(1));
         //pushPower = 25.0f;
         
     }
@@ -37,11 +48,33 @@ public class Player : MonoBehaviour
         
     }
     
+    /* Applies dmg points of damage to the player's health, and 
+     * returns current health of player, between 0 and maxHealth */
+    public int TakeDamage(int dmg) {
+        if (dmg < 0) {
+            Debug.Log("Error: Incorrect damage value given!");
+            return -1;
+        }
+        if ((currentHealth - dmg) < 0) {
+            currentHealth = 0; 
+            Debug.Log("You took " + dmg + " damage.");
+            Debug.Log("Current health: " + currentHealth);
+            return 0;
+        }
+        else {
+            currentHealth = currentHealth - dmg;
+            Debug.Log("You took " + dmg + " damage.");
+            Debug.Log("Current health: " + currentHealth);
+            return currentHealth;
+        }
+    
+    }
+    
     private void doMovement() {
         float verticalInput = Input.GetAxis("Vertical");
-        Debug.Log("Vertical: " + verticalInput);
+        //Debug.Log("Vertical: " + verticalInput);
         float horizontalInput = Input.GetAxis("Horizontal");
-        Debug.Log("Horizontal: " + horizontalInput);
+        //Debug.Log("Horizontal: " + horizontalInput);
 
         Vector3 up_direction = new Vector3(my_camera.transform.forward.x, 0, my_camera.transform.forward.z); 
         Vector3 right_direction = new Vector3(my_camera.transform.right.x, 0, my_camera.transform.right.z); 
