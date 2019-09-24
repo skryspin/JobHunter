f4bed6261ed1642c07c225546ab77f463bc4a9f3
skryspin <None>
2019-09-24 16:19:46 -0400

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private CharacterController characterController; 
+    private GameObject my_camera; 
+    private Vector3 lastMovement; 
+    private int score;
+    
+    public float speed;
+    public float jumpSpeed;
+    public float gravity;
+    public float pushPower; 
+
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = this.GetComponent<CharacterController>();
+        my_camera = GameObject.FindWithTag("MainCamera");
+        //pushPower = 25.0f;
+        
+    }
+
+    // Update is called once per frame
+    /* Note: Much of this code was referenced from the Unity manual */
+    void Update()
+    {
+        doMovement();        
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+    
+    private void doMovement() {
+        float verticalInput = Input.GetAxis("Vertical");
+        Debug.Log("Vertical: " + verticalInput);
+        float horizontalInput = Input.GetAxis("Horizontal");
+        Debug.Log("Horizontal: " + horizontalInput);
+
+        Vector3 up_direction = new Vector3(my_camera.transform.forward.x, 0, my_camera.transform.forward.z); 
+        Vector3 right_direction = new Vector3(my_camera.transform.right.x, 0, my_camera.transform.right.z); 
+        
+        Vector3 movement = up_direction * verticalInput + right_direction * horizontalInput;
+        movement *= speed;
+ 
+        if (characterController.isGrounded) {
+            if (Input.GetButtonDown("Jump"))  {
+                        //Debug.Log("jump detected!");
+                        movement.y = jumpSpeed;
+            }
+        }
+        else if (Input.GetButtonUp("Jump")){
+            if (lastMovement.y > 0) { //this is important - we don't want the velocity reset to 0 if he is already falling!
+                movement.y = 0; 
+                Debug.Log("STOP");
+            }
+            else {
+                movement.y = lastMovement.y;
+            }
+        }
+        else {
+            movement.y = lastMovement.y; 
+        }
+        
+        // Apply gravity. Gravity is multiplied by deltaTime twice (once here, and once below
+        // when the moveDirection is multiplied by deltaTime). This is because gravity should be applied
+        // as an acceleration (ms^-2)
+        movement.y -= gravity * Time.deltaTime;
+        //Debug.Log(movement.y);
+
+       
+        
+        
+        characterController.Move(movement * Time.deltaTime);
+        lastMovement = movement;
+    
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject collectable = other.gameObject;
+        //Debug.Log("Collision detected with: " + collectable);
+
+        
+        if (collectable.CompareTag("Collectable")) {
+            Destroy(collectable); 
+            score++;
+            Debug.Log(score);
+        }
+    }
+    
+
+    // this script pushes all rigidbodies that the character touches    
+    /* Function adapted from Unity documentation */
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        Rigidbody body = hit.collider.attachedRigidbody;
+
+        // no rigidbody
+        if (body == null || body.isKinematic)
+        {
+            return;
+        }
+
+        //// We dont want to push objects below us
+        //if (hit.moveDirection.y < -0.3)
+        //{
+        //    return;
+        //}
+
+        // Calculate push direction from move direction,
+        Vector3 pushDir = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
+
+        // If you know how fast your character is trying to move,
+        // then you can also multiply the push velocity by that.
+
+        // Apply the push
+        body.AddForce(pushDir * pushPower);
+    }
+}
---
+++ Assets/Scripts/ToggleObjectPositions.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleObjectPositions : MonoBehaviour
+{
+    [SerializeField] private GameObject parentOfPositionChildren; // should not change dynamically
+    private Component[] positions; //should not change dynamically
+    private int currentPositionIndex;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        positions = parentOfPositionChildren.GetComponentsInChildren<Transform>();
+        currentPositionIndex = 0; 
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        if (Input.GetButtonDown("CameraToggle")) {
+            currentPositionIndex++;
+            if (currentPositionIndex >= positions.Length) {
+                currentPositionIndex = 0;
+            }
+        
+        }
+        this.gameObject.transform.position = positions[currentPositionIndex].transform.position;
+        this.gameObject.transform.rotation = positions[currentPositionIndex].transform.rotation;
+        this.gameObject.transform.localScale = positions[currentPositionIndex].transform.localScale;
+    }
+}
