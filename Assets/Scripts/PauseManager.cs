60df00b483abf4919a47e070dc44b2630535f828
skryspin <None>
2020-01-17 13:05:49 -0500

    Closes #71

--- Assets/Scripts/PauseManager.cs
+++ Assets/Scripts/PauseManager.cs
@@ -18,14 +18,15 @@ public class PauseManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-        if (Input.GetButtonDown(buttonName)) {
-            if (SceneManager.GetSceneByName(sceneName).name == null) {
-                SceneManager.LoadScene(sceneName, LoadSceneMode.Additive); 
-            }
-            else {
-                //Debug.Log(SceneManager.GetSceneByName(sceneName).name); 
-                SceneManager.UnloadSceneAsync(sceneName); 
+        if (!player.isDead) {
+            if (Input.GetButtonDown(buttonName)) {
+                if (SceneManager.GetSceneByName(sceneName).name == null) {
+                    SceneManager.LoadScene(sceneName, LoadSceneMode.Additive); 
+                }
+                else {
+                    //Debug.Log(SceneManager.GetSceneByName(sceneName).name); 
+                    SceneManager.UnloadSceneAsync(sceneName); 
+                }
             }
         }
         
