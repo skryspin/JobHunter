1e8bf3d28985d45a5d2a0584aa789837c706b098
skryspin <None>
2020-02-06 23:52:35 -0500

    Started work on jumping algorithm. Added outline to the player :)

---
+++ Assets/RotateAlligorithm.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAlligorithm : MonoBehaviour
+{
+
+    public float normalSpeed; 
+    public float slowSpeed; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float speed;
+        if (this.transform.localRotation.z > -0.4 && this.transform.localRotation.z < 0.4) {
+            speed = slowSpeed;
+        }
+        else {
+            speed = normalSpeed;
+        }
+        transform.Rotate (0, 0,  ( speed * Time.deltaTime ), Space.Self );
+        Debug.Log(this.transform.localRotation.z);
+
+    }
+}
