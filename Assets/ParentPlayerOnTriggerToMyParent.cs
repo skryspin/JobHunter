886b9aeda0a1f5249a30d9642ee0cf9ac132637b
skryspin <None>
2020-03-05 21:14:58 -0500

    Fixed elevator jitter.

---
+++ Assets/ParentPlayerOnTriggerToMyParent.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParentPlayerOnTriggerToMyParent : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Player player = other.gameObject.GetComponent<Player>();
+        if (player != null) {
+            player.transform.parent = this.transform.parent;
+        }
+    }
+    
+    private void OnTriggerExit(Collider other) 
+    {
+        Player player = other.gameObject.GetComponent<Player>();
+        if (player != null) {
+            player.transform.parent = null;
+        }
+    }        
+}
---
+++ Assets/PointCameraOnTriggerEnter.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointCameraOnTriggerEnter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Puts the Main camera in pointmode if a Player enters this object's trigger
+    private void OnTriggerEnter(Collider other)
+    {
+        if (this.enabled) {
+            Player player = other.gameObject.GetComponent<Player>();
+            if (player != null) {
+                Camera.main.gameObject.GetComponent<FreeCamera>().pointmode = true;
+            }
+        }
+    }
+    
+    private void OnTriggerExit(Collider other)
+    {
+        if (this.enabled) {
+            Player player = other.gameObject.GetComponent<Player>();
+            if (player != null) {
+                Camera.main.gameObject.GetComponent<FreeCamera>().pointmode = false;
+            }
+        }
+
+    }
+}
